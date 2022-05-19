37cb6294fbff45e254df2fc38be479ec52fb3969
Lean65 <leandromelerio@gmail.com>
2022-05-19 11:08:39 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ margin: "0px" }}>NoCountry - Cohorte 5 Grupo 30</h1>
+    </div>
+  </React.StrictMode>
+);
