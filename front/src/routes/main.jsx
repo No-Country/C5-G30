35776166647255608ea1bd10867739ee2295727e
Lean65 <leandromelerio@gmail.com>
2022-05-19 11:17:07 -0300

    Lot of changes

---
+++ front/src/routes/main.jsx
@@ -0,0 +1,8 @@
+const Main = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ margin: "0px" }}>NoCountry - Cohorte 5 Grupo 30</h1>
+    </div>
+  );
+};
+export default Main;
