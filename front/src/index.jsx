043e432521fe59cf63024aea750095d749f7d9e3
Lean65 <leandromelerio@gmail.com>
2022-05-23 15:55:13 -0300

    router

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Main from "./routes/main";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Main />
+    <Router>
+      <Routes>
+        <Route path="" element={<Main />}></Route>
+        <Route path="/login" element="login"></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
--- prueba.js
+++
@@ -1,7 +0,0 @@
-const prueba = (a, b) => a + b;
-function algo(a, b) {
-  let num1 = a;
-  let num2 = b;
-  let resultado = a + b;
-  return resultado;
-}
