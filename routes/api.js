c50e9b2680aa7f55e1ca104773d6d820db2852c7
Lean65 <leandromelerio@gmail.com>
2022-05-24 15:52:34 -0300

    added log in

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/users')
+const User = require("../models/users");
 
+router.get("/", (req, res) => {
+  req.send("Conectado al servidor");
+});
 
-router.get('/getTurnos', async (req, res) => {
+router.get("/getTurnos", async (req, res) => {
+  const turnos = await User.find();
+  console.log("turnos");
+  //res.send("hola mundo")
+  res.json({
+    turnos: turnos,
+  });
+});
 
-    
-    const turnos = await User.find();
-    console.log("turnos")
-    //res.send("hola mundo")
-    res.json({
-        turnos: turnos
-    })
-})
-
-router.post("/turno", async (req, res,next) => {
-    const { name} = req.body;
-    const user = new User({
-        name: name
-    })
-    try {
-   
+router.post("/turno", async (req, res, next) => {
+  const { name } = req.body;
+  const user = new User({
+    name: name,
+  });
+  try {
     await user.save();
-    
-    res.send(user)
+
+    res.send(user);
     res.json({
-        status: "user agendado"
-    })
-}
-catch(err){
+      status: "user agendado",
+    });
+  } catch (err) {
     next(err);
-}
+  }
 });
 
 module.exports = router;
