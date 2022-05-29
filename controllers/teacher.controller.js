8e92ec84ce149691d3ec8f0f2997c26b1a354a49
Walter <walcruz1988.21@gmail.com>
2022-05-28 21:41:39 -0300

    controllersTeacher

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -10,6 +10,82 @@ const getTeacher= async (req, res) => {
     })
 }
 
+const getTeacherId=async (req, res) => {
+
+    try {
+        const buscado = await Teacher.findById(req.params.id)
+        res.send(buscado)
+        res.json({
+            buscado: buscado
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+const editTeacher=async (req, res) => {
+    const { firstName, lastName, dni, address, country, province, email, phone, status } = req.body
+    const newTeacher = {
+        firstName: firstName,
+        lastName: lastName,
+        dni: dni,
+        address: address,
+        country: country,
+        province: province,
+        email: email,
+        phone: phone,
+        status: status
+    }
+    try {
+        await Teacher.findByIdAndUpdate(req.params.id, newTeacher, { userFindModify: false })
+        res.json({
+            status: "teacher actualizado"
+        })
+
+    } catch (err) {
+
+    }
+}
+
+const addTeacher=async (req, res, next) => {
+
+    const { firstName, lastName, dni, address, country, province, email, phone, status } = req.body;
+    const teacher = new Teacher({
+        firstName: firstName,
+        lastName: lastName,
+        dni: dni,
+        address: address,
+        country: country,
+        province: province,
+        email: email,
+        phone: phone,
+        status: status
+    })
+
+    try {
+        const materia = await Materia.findById(req.params.id)
+        console.log(materia.name, "--->nombre")
+        teacher.mat = materia
+        if (teacher.materias) {
+            teacher.materias.push(materia.name)
+        }
+        await teacher.save()
+        materia.teachers.push(teacher)
+        await materia.save()
+        res.send(teacher)
+        res.json({
+            status: "teacher creada"
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
 module.exports={
-    getTeacher
+    getTeacher,
+    getTeacherId,
+    editTeacher,
+    addTeacher
 }
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -4,85 +4,17 @@ const Teacher = require('../database/models/teacher');
 const Materia = require('../database/models/materia');
 const Cohorte = require("../database/models/cohorte")
 const router = express.Router();
-const {getTeacher}=require("../controllers/teacher.controller")
+const {getTeacher,getTeacherId,editTeacher,addTeacher}=require("../controllers/teacher.controller")
 
-router.put("/editTeacher/:id", async (req, res) => {
-    const { firstName, lastName, dni, address, country, province, email, phone, status } = req.body
-    const newTeacher = {
-        firstName: firstName,
-        lastName: lastName,
-        dni: dni,
-        address: address,
-        country: country,
-        province: province,
-        email: email,
-        phone: phone,
-        status: status
-    }
-    try {
-        await Teacher.findByIdAndUpdate(req.params.id, newTeacher, { userFindModify: false })
-        res.json({
-            status: "teacher actualizado"
-        })
-
-    } catch (err) {
-
-    }
-})
+router.put("/editTeacher/:id", editTeacher)
 
 ////////////////////////TEACHER//////////////////////////////
 // solicita id de materia
-router.post("/addTeacher/:id", async (req, res, next) => {
-
-    const { firstName, lastName, dni, address, country, province, email, phone, status } = req.body;
-    const teacher = new Teacher({
-        firstName: firstName,
-        lastName: lastName,
-        dni: dni,
-        address: address,
-        country: country,
-        province: province,
-        email: email,
-        phone: phone,
-        status: status
-    })
-
-    try {
-        const materia = await Materia.findById(req.params.id)
-        console.log(materia.name, "--->nombre")
-        teacher.mat = materia
-        if (teacher.materias) {
-            teacher.materias.push(materia.name)
-        }
-        await teacher.save()
-        materia.teachers.push(teacher)
-        await materia.save()
-        res.send(teacher)
-        res.json({
-            status: "teacher creada"
-        })
-    }
-    catch (error) {
-        next(error)
-    }
-
-});
-
-
+router.post("/addTeacher/:id", addTeacher) 
 
 router.get('/getTeacher', getTeacher)
 
-router.get('/getTeacher/:id', async (req, res) => {
+router.get('/getTeacher/:id', getTeacherId)
 
-    try {
-        const buscado = await Teacher.findById(req.params.id)
-        res.send(buscado)
-        res.json({
-            buscado: buscado
-        })
-    }
-    catch (err) {
-        console.log(err)
-    }
-})
+
 module.exports = router;
