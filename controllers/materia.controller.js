9bab8869876a0e7350933bd5694a222b323aa4c1
Walcruz2021 <cruzherreria1@gmail.com>
2022-05-29 18:18:39 -0300

    controllerMateria

--- controllers/materia.controller.js
+++ controllers/materia.controller.js
@@ -1,7 +1,7 @@
-const express=require ("express")
-const Materia=require('../database/models/materia');
+const express = require("express")
+const Materia = require('../database/models/materia');
 
-const addMateria=async (req, res, next) => {
+const addMateria = async (req, res, next) => {
 
     const { registro, name, campo } = req.body
     const materia = new Materia({
@@ -16,9 +16,41 @@ const addMateria=async (req, res, next) => {
     })
 }
 
+const getMateria = async (req, res, next) => {
+    try {
+        await Materia.find({}, function (err, materias) {
+            Teacher.populate(materias, { path: "teachers" }, function (err, materias) {
+                res.json({
+                    materias: materias
+                })
+            })
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
 
+const editMateria = async (req, res, next) => {
+    const { registro, name, campo } = req.body
+    const newMateria = {
+        registro: registro,
+        name: name,
+        campo: campo
+    }
+    try {
+        await Materia.findByIdAndUpdate(req.params.id, newMateria, { userFindModify: false })
+        res.json({
+            status: "materia actualizada"
+        })
 
+    } catch (err) {
+        console.log(err)
+    }
 
-module.exports={
-    addMateria
+}
+
+module.exports = {
+    addMateria,
+    getMateria,
+    editMateria
 }
--- routes/materiaRoutes.js
+++ routes/materiaRoutes.js
@@ -2,41 +2,12 @@ const express=require("express")
 const Materia=require("../database/models/materia")
 const Teacher = require('../database/models/teacher');
 const router=express.Router()
-const {addMateria}=require("../controllers/materia.controller")
+const {addMateria,getMateria,editMateria}=require("../controllers/materia.controller")
 
 /////////////////////////MATERIA//////////////////////////////
-router.get('/getMateria', async (req, res, next) => {
-    try {
-        await Materia.find({}, function (err, materias) {
-            Teacher.populate(materias, { path: "teachers" }, function (err, materias) {
-                res.json({
-                    materias: materias
-                })
-            })
-        })
-    } catch (err) {
-        console.log(err)
-    }
-})
+router.get('/getMateria', getMateria )
 
-router.put("/editMateria/:id", async (req, res, next) => {
-    const { registro, name, campo } = req.body
-    const newMateria = {
-        registro: registro,
-        name: name,
-        campo: campo
-    }
-    try {
-        await Materia.findByIdAndUpdate(req.params.id, newMateria, { userFindModify: false })
-        res.json({
-            status: "materia actualizada"
-        })
-
-    } catch (err) {
-        console.log(err)
-    }
-
-})
+router.put("/editMateria/:id", editMateria)
 
 router.post('/addMateria', addMateria)
 
