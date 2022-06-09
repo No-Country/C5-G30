aa980e42680268991377881bad955bfd21f6b07a
Walter <walcruz1988.21@gmail.com>
2022-06-09 20:54:13 -0300

    merge

--- database/models/teacher.js
+++ database/models/teacher.js
@@ -3,19 +3,7 @@ const {Schema}=mongoose
 
 
 const teacher=new Schema({
-<<<<<<< HEAD
-    
-    firstName:{type:String,require:true},
-    lastName:{type:String,require:true},
-    dni:{type:Number,require:true},
-    address:{type:String, require:true},
-    country:{type:String, require:true},
-    province:{type:String, require:true},
-    email:{type:String, require:true},
-    phone:{type:Number, require:true},
-    status:{type:Boolean, require:true},
-    materias:[{type:String}]
-=======
+
     firstName:{type:String,require:false},
     lastName:{type:String,require:false},
     dni:{type:Number,require:false},
@@ -27,7 +15,7 @@ const teacher=new Schema({
     status:{type:Boolean, require:false},
     materias:[{type:String}],
     password:{type:String,require:false}
->>>>>>> 4abb532ea6107e2339b92548ddad9cd94e326fba
+
 })
 
 module.exports=mongoose.model('Teacher',teacher)
