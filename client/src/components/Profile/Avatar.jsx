9b4b77e2b27368aa2939ae074df440943dd68411
Exequiel65 <marcos97britos@gmail.com>
2022-06-13 13:05:39 -0300

    add previsualitation image avatar

--- client/src/components/Profile/Avatar.jsx
+++ client/src/components/Profile/Avatar.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 const Avatar = () => {
+  const handleChange = () => {
+    let $img = document.querySelector("#image-avatar");
+    let $inputFile = document.querySelector("#avatar");
+    let button = document.querySelector('button')
+    const archivos = $inputFile.files;
+    if (!archivos || !archivos.length) {
+      $img.src = "";
+      return;
+    }
+    const primerArchivo = archivos[0];
+    const objectURL = URL.createObjectURL(primerArchivo);
+    $img.src = objectURL;
+    button.style.display = "block"
 
-  
+  };
 
   return (
     <div className="avatar-container">
@@ -10,12 +23,13 @@ const Avatar = () => {
         <img
           src="/web-developer-design-vector-5884837.jpg"
           alt="foto de perfil"
+          id="image-avatar"
         />
       </div>
       <form className="form-avatar">
-        <label htmlFor="avatar">Cambiar Foto</label>
-        <input id="avatar" type="file" />
-        <button type="submit">Cambiar foto</button>
+        <label htmlFor="avatar">Cambiar foto</label>
+        <input id="avatar" type="file" name="avatar" onChange={handleChange} />
+        <button type="submit" className="btn">Guardar</button>
       </form>
     </div>
   );
